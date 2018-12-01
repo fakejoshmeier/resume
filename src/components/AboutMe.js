c775889436b682539a82ec4054a7649d1eb397be
Joshua Meier <joshua.meier2@gmail.com>
2018-11-30 16:53:39 -0800

    Added profile pic, links, tab structure

---
+++ src/components/AboutMe.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import './styles/AboutMe.css';
+import profile from '../assets/jmeier.jpg'
+
+class AboutMe extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			contact: false,
+			sent: false,
+			error: false,
+		};
+	}
+
+	render() {
+		return (
+			<div className='aboutMe'>
+				<img src={ profile } alt="It's Me!" className="profilePic"/>
+				<p className='profileText'>Arma virumque canō, Trōiae quī prīmus ab ōrīs/Ītaliam, fātō profugus, Lāvīniaque vēnit/lītora, multum ille et terrīs iactātus et altō/vī superum saevae memorem Iūnōnis ob īram;/multa quoque et bellō passūs, dum conderet urbem,/inferretque deōs Latiō, genus unde Latīnum,/Albānīque patrēs, atque altae moenia Rōmae./Mūsa, mihī causās memorā, quō nūmine laesō,/quidve dolēns, rēgīna deum tot volvere cāsūs/īnsīgnem pietāte virum, tot adīre labōrēs/impulerit. Tantaene animīs caelestibus īrae?/Urbs antīqua fuit, Tyriī tenuēre colōnī,/Karthāgō, Ītaliam contrā Tiberīnaque longē/ōstia, dīves opum studiīsque asperrima bellī,/quam Iūnō fertur terrīs magis omnibus ūnam/posthabitā coluisse Samō; hīc illius arma,/hīc currus fuit; hōc rēgnum dea gentibus esse,/sī quā Fāta sinant, iam tum tenditque fovetque./Prōgeniem sed enim Trōiānō ā sanguine dūcī/audierat, Tyriās olim quae verteret arcēs;/hinc populum lātē regem bellōque superbum/ventūrum excidiō Libyae: sīc volvere Parcās./Id metuēns, veterisque memor Sāturnia bellī,/prīma quod ad Trōiam prō cārīs gesserat Argīs—/necdum etiam causae īrārum saevīque dolōrēs</p>
+			</div>
+		)
+	}
+}
+
+export default AboutMe;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react';
+import AboutMe from './AboutMe';
 
-//CSS import needed here
 import './styles/Main.css';
+
+//const projects = require("json-loader!yaml-loader!../projects.yaml");
+
+const Header = ({ tab, callback }) => (
+	<header>
+		<nav>
+		<span onClick={ tab !== 0 ? () => callback() : 0 } className={ tab === 0 ? 'meTabS' : 'meTab' }>Josh Meier</span>
+		<span onClick={ tab !== 1 ? () => callback() : 0 } className={ tab === 1 ? 'projTabS' : 'projTab' }>Projects</span>
+		<span className='icons'>
+			<a href="https://github.com/fakejoshmeier">G</a> | <a href="https://www.linkedin.com/in/joshua-meier2/">L</a>
+		</span>
+		</nav>
+	</header>
+)
 
 class Main extends Component {
 	constructor() {
 		super();
 		this.state = {
-			selected: 0,
+			selected: 0, //Which project has been selected
+			tab: 0, //Which tab has been selected.  It's either About or Projects
 		};
 	}
+
+	changeTab = () => {
+		this.setState({
+			tab: !this.state.tab
+		});
+	}
+
+	changeSelection = ( idx ) => {
+		this.setState({
+			selected: parseInt(idx)
+		});
+	}
+
 	render() {
 		return (
 			<div className='mainContainer'>
-				<p>Josh Meier</p>
+			<Header tab={ this.state.tab } callback={ () => this.changeTab }/>
+						<AboutMe/>
 			</div>
+//				{ this.state.tab === 0 ?
+//					:
+//					<main className='infoContainer'>
+//						<ProjectList projects={projects} selected={this.state.selected} />
+//						<ProjectBody project={projects[this.state.selected]} />
+//					</main>
+//				}
 		)
 	}
 }
